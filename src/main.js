8cb70a9e695d25063e5293d73a177fd0c0f2f879
Mauricio <mauricio.pereira@mobly.com.br>
2022-06-04 02:23:53 -0300

    feat: add mitt lib

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import routers from './routers'
 import './tailwind.css'
 import './assets/scss/modules/_icons.scss'
 import { store } from './store/store'
+import mitt from 'mitt'
+
+const emitter = mitt()
 
 const app = createApp(App)
 
+app.config.globalProperties.emitter = emitter
 app.use(store)
 app.use(routers)
 app.mount('#app')
