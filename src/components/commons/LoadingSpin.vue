9a8ff738261abcb28d67b5a9f99f14a3d1ba3a2f
Mauricio <mauricio.pereira@mobly.com.br>
2022-06-04 02:24:41 -0300

    feat: commons components

---
+++ src/components/commons/LoadingSpin.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="flex items-center justify-center w-full space-x-4"
+    :class="py"
+  >
+    <span class="w-5 h-5 mr-1 -ml-1 text-2xl text-bertolt-text animate-spin icon-star_rate"></span>
+    <span
+      v-if="text"
+      class="text-bertolt-text"
+    >
+      {{ text }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingSpin',
+
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    py: {
+      type: String,
+      default: 'py-10'
+    }
+  }
+}
+</script>
