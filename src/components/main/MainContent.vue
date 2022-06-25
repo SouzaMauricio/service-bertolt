e182f600ee89237a674cfaaecc7ae3c3ff028242
Mauricio <mauricio.pereira@mobly.com.br>
2022-06-25 01:59:23 -0300

    feat: add images

--- src/components/main/MainContent.vue
+++ src/components/main/MainContent.vue
@@ -1,12 +1,17 @@
 <template>
   <div
-    class="flex items-center justify-center w-full pt-40"
+    class="grid items-center w-full pt-40 justify-items-center"
   >
     <p
       class="text-3xl opacity-75 text-bertolt-primary"
     >
       Bem-vindo(a), tenha um ótimo trabalho ;)
     </p>
+    <img
+      :src="require('../../assets/high-five-bro.png')"
+      class="h-96"
+      alt=""
+    >
   </div>
 </template>
 
--- src/views/DefaultScreen.vue
+++ src/views/DefaultScreen.vue
@@ -1,12 +1,17 @@
 <template>
   <div
-    class="flex items-center justify-center w-full pt-40"
+    class="grid items-center w-full pt-40 justify-items-center"
   >
     <p
       class="text-3xl opacity-75 text-bertolt-primary"
     >
       Está no lugar certo??? Não há nada por aqui...
     </p>
+    <img
+      :src="require('../assets/no-data-bro.png')"
+      class="h-96"
+      alt=""
+    >
   </div>
 </template>
 
